f9adc232a09b4f8e75e586bd15316828b6f980d7
Henrique Sandim <fuzzy.bot1708@gmail.com>
2023-12-13 00:26:44 -0300

    Pasta de constants feita

---
+++ src/constants/data.ts
@@ -0,0 +1,8 @@
+export const enterpriseData = {
+  name: "Aplicação",
+  links: {
+    facebook: "https://google.com.br",
+    instagram: "https://google.com.br",
+    linkedin: "https://google.com.br",
+  },
+};
